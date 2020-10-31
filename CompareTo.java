a0db075665ca4bd69c0847211794c0d9aa1a6793
nkyada <57616167+nkyada@users.noreply.github.com>
2020-10-31 14:05:45 +0530

    Update CompareTo.java

--- CompareTo.java
+++ CompareTo.java
@@ -7,9 +7,9 @@ public class CompareTo
 		String s3="meklo";
 		String s4="hemlo";
 		String s5="flag";
-		System.out.println(s1.compareTo(s2));//0 because both are equal
-		System.out.println(s1.compareTo(s3));//-5 because "h" is 5 times lower than "m"
-		System.out.println(s1.compareTo(s4));//-1 because "l" is 1 times lower than "m"
-		System.out.println(s1.compareTo(s5));//2 because "h" is 2 times greater than "f"
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.compareTo(s4));
+		System.out.println(s1.compareTo(s5));
 	}
 }
