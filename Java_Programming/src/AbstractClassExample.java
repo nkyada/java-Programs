3414ce54b849d6bfa9cf90123eafee494d4497fa
nkyada <57616167+nkyada@users.noreply.github.com>
2020-12-19 22:06:47 +0530

    Add files via upload

--- Java_Programming/src/AbstractClassExample.java
+++ Java_Programming/src/AbstractClassExample.java
@@ -1,9 +1,21 @@
-
+abstract class Mathematical
+{
+	int a = 30 , b = 40;
+	public abstract void add();
+}
+class Operation extends Mathematical
+{
+	public void add()
+	{
+		System.out.println("sum of a and b is :"+ a+b);
+	}
+}
 public class AbstractClassExample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		Mathematical obj = new Operation();
+		obj.add();
 	}
 
 }
